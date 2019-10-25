57cce3ee881f8de625e6920370083e8f2837fc4b
huytx0909 <huytx0909@gmail.com>
2019-10-25 23:03:29 +0700

    init template

---
+++ js/custom/navbar.js
@@ -0,0 +1,23 @@
+var url = 'https://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+		  'apiKey=061a6658eb144ee4938a594a320c9cbc';
+		  
+var req = new Request(url);
+fetch(req)
+.then(function(response) {
+ console.log(response.json());
+})
+
+
+
+// fetch('https://reqres.in/api/users?page=2').then(response => response.json())
+// .then(data => {
+// 	var liNode = document.createElement("li");
+// 	var aNode = document.createElement("a");
+
+// 	aNode.value = data.data[1];
+
+// 	document.getElementById("pages-dropdown").appendChild(liNode);
+
+// 	console.log(data);
+// })
